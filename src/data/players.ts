c5995bb9666183c8b7e1a81ea8f2f7e9e8115daa
yolofolo130-ship-it <yolofolo130@gmail.com>
2026-08-07 07:56:36 -0400

    Update player positions for Vegas Golden Knights

--- src/data/players.ts
+++ src/data/players.ts
@@ -34,12 +34,12 @@ export const skaters: Skater[] = [
   { id: "uta-3", name: "Cole Marsh", teamId: "utah-mammoths", position: "D", number: 2, gp: 5, goals: 1, assists: 5, pim: 0 },
   { id: "uta-4", name: "Ryder James", teamId: "utah-mammoths", position: "D", number: 25, gp: 5, goals: 1, assists: 3, pim: 0 },
 
-  { id: "vgk-1", name: "Sinny", teamId: "vegas-golden-knights", position: "RW/C", number: 77, gp: 3, goals: 5, assists: 1, pim: 2 },
+  { id: "vgk-1", name: "Sinny", teamId: "vegas-golden-knights", position: "RW", number: 77, gp: 3, goals: 5, assists: 1, pim: 2 },
   { id: "vgk-2", name: "Butter", teamId: "vegas-golden-knights", position: "LW", number: 13, gp: 3, goals: 1, assists: 3, pim: 0 },
   { id: "vgk-3", name: "Twizzy", teamId: "vegas-golden-knights", position: "C", number: 8, gp: 3, goals: 2, assists: 1, pim: 2 },
-  { id: "vgk-4", name: "Mesh", teamId: "vegas-golden-knights", position: "C/LW", number: 16, gp: 3, goals: 0, assists: 0, pim: 0 }, 
-  { id: "vgk-5", name: "Caleb", teamId: "vegas-golden-knights", position: "D/LW", number: 9, gp: 3, goals: 1, assists: 0, pim: 0 },
-  { id: "vgk-6", name: "Sidedeer", teamId: "vegas-golden-knights", position: "D/RW", number: 38 , gp: 2, goals: 1, assists: 0, pim: 0 }, 
+  { id: "vgk-4", name: "Mesh", teamId: "vegas-golden-knights", position: "C", number: 16, gp: 3, goals: 0, assists: 0, pim: 0 }, 
+  { id: "vgk-5", name: "Caleb", teamId: "vegas-golden-knights", position: "D", number: 9, gp: 3, goals: 1, assists: 0, pim: 0 },
+  { id: "vgk-6", name: "Sidedeer", teamId: "vegas-golden-knights", position: "LW", number: 38 , gp: 2, goals: 1, assists: 0, pim: 0 }, 
   { id: "vgk-7", name: "Drago", teamId: "vegas-golden-knights", position: "D", number: 99, gp: 0, goals: 0, assists: 0, pim: 0},
 
   { id: "stl-1", name: "Nolan Ashby", teamId: "st-louis-blues", position: "C", number: 14, gp: 5, goals: 3, assists: 4, pim: 4 },
