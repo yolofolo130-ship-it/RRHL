e0acc47ff33fe9d40386b614b63f9650f68be2a0
Rec Room Hockey League <yolofolo130@gmail.com>
2026-08-09 23:56:53 -0400

    Update skater roster info

--- src/data/players.ts
+++ src/data/players.ts
@@ -79,12 +79,12 @@ export const skaters: Skater[] = [
   { id: "vgk-10", name: "Sigma3k", teamId: "vegas-golden-knights", position: "C", number: 35, gp: 0, goals: 0, assists: 0, pim: 0, overall: 73, flag: "DR" },
 
   { id: "stl-1", name: "Vengeance", teamId: "st-louis-blues", position: "C", number: 14, gp: 3, goals: 4, assists: 0, pim: 0, overall: 92, star: "Wheels", flag: "Canada" },
-  { id: "stl-2", name: "Augy", teamId: "st-louis-blues", position: "RW", number: 21, gp: 0, goals: 0, assists: 0, pim: 0, overall: 80, flag: "USA" },
+  { id: "stl-2", name: "Augy", teamId: "st-louis-blues", position: "RW", number: 21, gp: 1, goals: 0, assists: 0, pim: 0, overall: 80, flag: "USA" },
   { id: "stl-3", name: "EvanTheGuy", teamId: "st-louis-blues", position: "LW", number: 4, gp: 0, goals: 0, assists: 0, pim: 0, overall: 86, flag: "USA" },
   { id: "stl-4", name: "Fire", teamId: "st-louis-blues", position: "C", number: 7, gp: 0, goals: 0, assists: 0, pim: 0, overall: 93, star: "Quick Release", flag: "USA" },
   { id: "stl-5", name: "Tidy", teamId: "st-louis-blues", position: "LW", number: 2, gp: 0, goals: 0, assists: 0, pim: 0, overall: 82, flag: "USA" },
   { id: "stl-6", name: "TDLMarcus", teamId: "st-louis-blues", position: "D", number: 56, gp: 0, goals: 0, assists: 0, pim: 0, overall: 79, flag: "USA" },
-  { id: "stl-7", name: "Hogeye", teamId: "st-louis-blues", position: "C", number: 69, gp: 0, goals: 0, assists: 0, pim: 0, overall: 79, flag: "USA" },
+  { id: "stl-7", name: "Hogeye", teamId: "st-louis-blues", position: "C", number: 69, gp: 2, goals: 0, assists: 0, pim: 0, overall: 79, flag: "USA" },
   { id: "stl-8", name: "AlexKiller", teamId: "st-louis-blues", position: "LW", number: 1, gp: 0, goals: 0, assists: 0, pim: 0 },
 
   { id: "lak-1", name: "MVP", teamId: "los-angeles-kings", position: "C", number: 9, gp: 2, goals: 13, assists: 2, pim: 0, overall: 85, flag: "USA" },
