3e1f6fddd03af837a923049da577a4994a92c5ec
Rec Room Hockey League <yolofolo130@gmail.com>
2026-08-14 04:51:24 -0400

    Update DDino's goalie game logs

--- src/data/gameLogs.ts
+++ src/data/gameLogs.ts
@@ -522,7 +522,7 @@ export const goalieGameStatLines: GoalieGameStatLine[] = [
   // ---------- NEW YORK RANGERS ----------
   { playerName: "DDino", gameId: "g02", gs: 0, dec: "L", shotsAgainst: 0, goalsAgainst: 0, shutout: 0, goals: 0, assists: 0, points: 0, pim: 0 },
   { playerName: "DDino", gameId: "g10", gs: 1, dec: "OTL", shotsAgainst: 41, goalsAgainst: 2, shutout: 0, goals: 0, assists: 0, points: 0, pim: 0 },
-  { playerName: "DDino", gameId: "g15", gs: 0, dec: "W", shotsAgainst: 4, goalsAgainst: 4, shutout: 0, goals: 0, assists: 0, points: 0, pim: 0 },
+  { playerName: "DDino", gameId: "g15", gs: 0, dec: "W", shotsAgainst: 0, goalsAgainst: 0, shutout: 0, goals: 0, assists: 0, points: 0, pim: 0 },
   { playerName: "DDino", gameId: "g18", gs: 0, dec: "W", shotsAgainst: 21, goalsAgainst: 3, shutout: 0, goals: 0, assists: 0, points: 0, pim: 0 },
 
   // ---------- UTAH MAMMOTHS ----------
